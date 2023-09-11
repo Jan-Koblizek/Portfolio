61745f09b3792acf9db54197f2bd47a8b5aed174
Jan-Koblizek <48485433+Jan-Koblizek@users.noreply.github.com>
2023-09-11 16:27:13 +0200

    Init

---
+++ portfolio.js
@@ -0,0 +1,25 @@
+function blurAndDarkenBackground() {
+    document.getElementById("overlay").style.visibility = "visible";
+    document.getElementById("overlay").style.opacity = 1;
+}
+
+function removeBackgroundBlur() {
+    document.getElementById("overlay").style.visibility = "hidden";
+    document.getElementById("overlay").style.opacity = 0;
+}
+
+function displayInfo(card) {
+    blurAndDarkenBackground();
+    var popups = card.querySelectorAll('.popup');
+    popups[0].style.visibility = "visible";
+    popups[0].style.opacity = 1;
+}
+
+function hideInfo() {
+    var popups = document.querySelectorAll(".popup");
+    for (i=0; i<popups.length; i++) {
+        popups[i].style.opacity = 0;
+        popups[i].style.visibility = "hidden";
+    }
+    removeBackgroundBlur();
+}
